1ce74a587ef74aa845f7cd72470d4be8a36e894b
RohitVemparala <39242712+RVKarmani@users.noreply.github.com>
2020-04-23 23:24:17 +0530

    Update final_status.sh

--- final_status.sh
+++ final_status.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#VARIANT 2 - ITERATING OVER ADDRESSES
-IP=( `jq -r ".[].Ip " services.json` )
-NAMES=( `jq -r ".[].Name " services.json` )
-EMAIL_BODY=""
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+IP=( `jq -r ".[].Ip " $DIR/services.json` )
+NAMES=( `jq -r ".[].Name " $DIR/services.json` )
 
 for ix in ${!IP[@]}
 do
-	STATUS=`./wait-for-it.sh ${IP[$ix]} --strict -- echo "UP"`
+	STATUS=`$DIR/wait-for-it.sh ${IP[$ix]} -t 600 --strict -- echo "UP"`
 	if [ "$STATUS" == "UP" ]
 	then
 		echo "${NAMES[$ix]} is UP"
