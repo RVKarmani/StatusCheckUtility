0b14348b721ec3c327e58b998a4879c40e2acf85
RohitVemparala <39242712+RVKarmani@users.noreply.github.com>
2020-04-23 23:26:51 +0530

    Update final_status.sh

--- final_status.sh
+++ final_status.sh
@@ -2,7 +2,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 IP=( `jq -r ".[].Ip " $DIR/services.json` )
 NAMES=( `jq -r ".[].Name " $DIR/services.json` )
-
 for ix in ${!IP[@]}
 do
 	STATUS=`$DIR/wait-for-it.sh ${IP[$ix]} -t 600 --strict -- echo "UP"`
