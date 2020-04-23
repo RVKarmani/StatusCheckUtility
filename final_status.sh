75a6124ba3f5ed6913bf08ec61f2d95665f92a56
Rohit Vemparala <ubuntu@ip-10-0-2-140.ec2.internal>
2020-04-23 04:15:23 +0000

    Added in servers instance

--- final_status.sh
+++ final_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #VARIANT 2 - ITERATING OVER ADDRESSES
-IP=( `jq -r ".[].Ip " microservice.json` )
-NAMES=( `jq -r ".[].Name " microservice.json` )
+IP=( `jq -r ".[].Ip " services.json` )
+NAMES=( `jq -r ".[].Name " services.json` )
 EMAIL_BODY=""
 
 for ix in ${!IP[@]}
@@ -12,6 +12,6 @@ do
 		echo "${NAMES[$ix]} is UP"
 	else
 		echo "${NAMES[$ix]} is DOWN"
-		echo "${NAMES[$ix]} is DOWN" | mutt -s "Gamification Service Check" -a ../logs/"${NAMES[$ix]}.log" -- vemrohit@publicisgroupe.net
+		echo "${NAMES[$ix]} is DOWN" | mutt -s "Gamification  Servers Check" -- vemrohit@publicisgroupe.net
 	fi
 done
